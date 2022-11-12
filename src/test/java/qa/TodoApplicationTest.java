a873057dd5ab30cc8d704df6da44b7b2656e4a0c
Hemant Hari Kumar <hemant.hari.kumar@gmail.com>
2022-11-11 23:39:43 -0500

    Add test cases using test doubles (mocks)

--- src/test/java/qa/TodoApplicationTest.java
+++ src/test/java/qa/TodoApplicationTest.java
@@ -1,17 +1,17 @@
 package qa;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 
 class TodoApplicationTest {
 
@@ -24,22 +24,45 @@ class TodoApplicationTest {
 
 	@BeforeEach
 	void setup() {
-		//TODO
+		String dummyUsername = "dummyUsername1";
+		personServiceMock = mock(PersonService.class);
+		todoServiceMock = mock(TodoService.class);
+		Mockito.lenient().when(personServiceMock.findUsernameById(123L)).thenReturn(dummyUsername);
+		Mockito.lenient().when(personServiceMock.findUsernameById(123L)).thenReturn("noDataUsername");
+		Mockito.lenient().when(todoServiceMock.addTodo(dummyUsername, "newTodo")).thenReturn(true);
+		Mockito.lenient().when(todoServiceMock.retrieveTodos(dummyUsername)).thenReturn(new ArrayList<>(Arrays.asList(
+				"todo1",
+				"todo2",
+				"todo3"
+		)));
+		Mockito.lenient().when(todoServiceMock.retrieveTodos("noDataUsername")).thenReturn(new ArrayList<>());
+		Mockito.doNothing().when(todoServiceMock).completeTodo("todo1");
+		Mockito.doNothing().when(todoServiceMock).completeTodo("todo2");
+		Mockito.doNothing().when(todoServiceMock).completeTodo("todo3");
+
+		todoApp = new TodoApplication(todoServiceMock, personServiceMock);
 	}
 
 	@Test
 	void addTodoTest() {
-		//TODO
+		Assertions.assertTrue(todoApp.addTodo(123L, "newTodo"));
+		Mockito.verify(todoServiceMock).addTodo("dummyUsername1", "newTodo");
 	}
 
 	@Test
 	void completeAllTodosTest() {
-		//TODO
+		todoApp.completeAllTodos(123L);
+		Mockito.verify(todoServiceMock).retrieveTodos("dummyUsername1");
+		Mockito.verify(todoServiceMock).completeTodo("todo1");
+		Mockito.verify(todoServiceMock).completeTodo("todo2");
+		Mockito.verify(todoServiceMock).completeTodo("todo1");
+
 	}
 
 	@Test
 	void completeAllTodosWithNoNotes() {
-		//TODO
+		todoApp.completeAllTodos(234L);
+		Mockito.verify(todoServiceMock, never()).completeTodo(any());
 	}
 
 
